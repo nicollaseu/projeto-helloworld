4c16856e24ceaa3b900bf8e435e70a821e4e42b7
Nicollas <nsilvademoraes@gmail.com>
2024-07-01 12:38:52 -0300

    Criando 2 Endpoint: BsmGeneration

--- src/main/java/com/generation/helloWorld/controller/HelloWorldController.java
+++ src/main/java/com/generation/helloWorld/controller/HelloWorldController.java
@@ -5,11 +5,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
     @RestController
-    @RequestMapping("/hello")
+    @RequestMapping("/task1")
     public class HelloWorldController {
 
     @GetMapping
         public String helloWorld() {
         return "Hello World!!!";
     }
+
+    @GetMapping("/bsm")
+        public String BsmGeneration() {
+        return "Mentalidade de Crescimento (Growth Mindset)\n" +
+               "Responsabildade Pessoal (Personal Responsability)\n" +
+               "Orientação ao Futuro (Future Orientation)\n" +
+               "Comunicação (Communication)\n" +
+               "Trabalho em Equipe (Teamwork)\n" +
+               "Pensamento Crítico (Critical Thinking)\n" +
+               "Resolução de Problemas (Problem Solving)\n" +
+               "Orientação para Resultados (Results Orientation)\n" +
+               "Gerenciamento de Tempo (Time Management)\n" +
+               "Adaptabilidade (Adaptability)\n" +
+               "Autoconfiança (Self-Confidence)\n" +
+               "Ética e Integridade (Ethics and Integrity)";
+    }
+
 }
